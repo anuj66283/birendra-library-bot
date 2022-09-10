5561dca6f6821b8227b57534fb99606faeef0bfa
anuj66283 <anujbhattarai66283@gmail.com>
2022-09-10 21:36:47 +0545

    added author search

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ async def search(event):
     sender = await event.get_sender()
     a = ""
     for inf in cor:
-        msg = f'**Bookid: {inf["BookID"]}**\nTitle: {inf["Title"]}\nPublisher: {inf["Publisher"]}\nPlace: {inf["Place"]}\nYear: {inf["YEAR"]}\n---------------------------------------------------\n'
+        msg = f'**Bookid: {inf["BookID"]}**\nTitle: {inf["Title"]}\nPublisher: {inf["Publisher"]}\nAuthor: {inf["ResTitle"]}\nPlace: {inf["Place"]}\nYear: {inf["YEAR"]}\n---------------------------------------------------\n'
         a += msg
 
         #cannot send more than 4000 characters so this condition is used
@@ -54,11 +54,35 @@ async def search(event):
 
     await bot.send_message(sender.id, a)
 
+@bot.on(events.NewMessage(pattern=r"\/search \""))
+async def author_search(event):
+    sender = await event.get_sender()
+    txt = event.raw_text
+    txt = txt.split("\"")
+    
+    if len(txt)<4:
+        await bot.send_message(sender.id, "Please send message correctly")
+    
+    else:
+        ttl = [x for x in coll.find({'$text': {'$search': txt[1]}})]
+        auth = [x for x in coll.find({'$text': {'$search': txt[3]}})]
+        rtn = []
+
+        for i in range(abs(len(ttl)-len(auth))):
+            if ttl[i] in auth:
+                rtn.append(ttl[i])
+        msg = ""
+        for inf in rtn:
+            msg += f'**Bookid: {inf["BookID"]}**\nTitle: {inf["Title"]}\nPublisher: {inf["Publisher"]}\nAuthor: {inf["ResTitle"]}\nPlace: {inf["Place"]}\nYear: {inf["YEAR"]}\n---------------------------------------------\n'
+        
+        await bot.send_message(sender.id, msg)
+        
+
 @bot.on(events.NewMessage(pattern=r"\/detail "))
 async def detail(event):
     sender = await event.get_sender()
     txt = event.raw_text
-
+    print(f'{txt} !!!!! {sender.username}')
     txt = txt.split(' ')[1:]
     sz = len(txt)
     if sz>1 or sz==0:
@@ -70,16 +94,15 @@ async def detail(event):
         await bot.send_message(sender.id, "Extracting data it may take some time")
         await bot.send_message(sender.id, msg)
 
-@bot.on(events.NewMessage(pattern=r"\/start"))
-async def greet(event):
-    sender = await event.get_sender()
-    await bot.send_message(sender.id, f"Type /help for help\nSource code")
-
 @bot.on(events.NewMessage(pattern=r"\/help"))
 async def help(event):
     sender = await event.get_sender()
-    msg = 'List of commands:\n"/search {bookname}" to search the book by name\n"/detail {bookid}" to get detail of the book. You will get book id from search command.'
+    msg = 'List of commands:\n"/search {bookname}" to search the book by name\n\'/search "{book_name}" "{author}"\' to search for book by author\n/detail {bookid}" to get detail of the book. You will get book id from search command'
     await bot.send_message(sender.id, msg)
 
+@bot.on(events.NewMessage(pattern=r"\/start"))
+async def greet(event):
+    sender = await event.get_sender()
+    await bot.send_message(sender.id, f"Type /help for help\nSource code https://github.com/anuj66283/birendra-library-bot")
 
 bot.run_until_disconnected()
